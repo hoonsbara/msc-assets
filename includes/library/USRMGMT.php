3582bb8738f74094d38bad2384aab79fad399bed
ThomasRudd <ThomasRudd@a594294e-8a34-11de-a519-b78fc8232508>
2009-08-23 19:59:55 +0000

    User management class file

---
+++ includes/library/USRMGMT.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * includes/library/USRMGMT.php
+ * 
+ * Various User management functions
+ * 
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @package MSC Assets
+ * @todo clean this up into a class
+ * 
+ */
+defined('_MSC') or die('Unauthorized');
+$debug->log("User Management Class initiating...");
+
+
+
+?>
